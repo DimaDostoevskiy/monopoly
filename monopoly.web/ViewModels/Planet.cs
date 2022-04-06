1701eae0f63db94af25a51fc1b07983c4f26f172
DimaDostoevskiy <dostoevskiy@list.ru>
2022-04-06 20:09:46 +0300

    Merge branch 'WelkomToPlanet'

--- monopoly.web/ViewModels/Planet.cs
+++ monopoly.web/ViewModels/Planet.cs
@@ -2,8 +2,9 @@ namespace monopoly.web.ViewModels
 {
     public class Planet : BaseModel
     {
-        public Planet(int x, int y, string name, string imagePath, string cssClass)
+        public Planet(int id, int x, int y, string name, string imagePath, string cssClass)
         {
+            Id = id;
             Name = name;
             PositionX = x;
             PositionY = y;
